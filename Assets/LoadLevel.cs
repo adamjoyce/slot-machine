2800edecf6a3c1372a316f264a06df71599a1182
Adam Joyce <adamjoyce@outlook.com>
2016-02-11 09:46:44 +0000

    Attempting to fix my local repo

---
+++ Assets/LoadLevel.cs
@@ -0,0 +1,13 @@
+﻿/*using UnityEngine;
+using System.Collections;
+
+public class LoadLevel : MonoBehaviour {
+
+  public void LoadLevel(int index) {
+    Application.LoadLevel(index);
+  }
+
+  public void LoadLevel(string levelName) {
+    Application.LoadLevel(levelName);
+  }
+}*/
