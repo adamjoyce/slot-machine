3dd0b25f381e7eba2efdcce140944da2205eb3c3
Adam Joyce <adamjoyce@outlook.com>
2016-03-13 14:46:58 +0000

    Added death zone for earth level and enemy units can now die on them - does not work for spitter

---
+++ Assets/Scripts/DeathZoneDamage.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathZoneDamage : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Kill anything that touches the lava.
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player") {
+            coll.gameObject.GetComponent<PlayerController>().inflictDamage(50, gameObject.GetComponent<Transform>().position);
+        } else if (coll.gameObject.tag == "Enemy") {
+            coll.gameObject.GetComponent<Enemy>().inflictDamage(50);
+        }
+    }
+}
