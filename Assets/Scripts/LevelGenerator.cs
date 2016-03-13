a860fc60e48c0d3f431c76188bb7a795354f6c00
Adam Joyce <adamjoyce@outlook.com>
2016-03-13 11:09:48 +0000

    Modified charger and spitter spawning

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -5,189 +5,196 @@ using System.Collections.Generic;
 public class LevelGenerator : MonoBehaviour
 {
 
-  public GameObject platformPrefab;
-  public GameObject playerPrefab;
-  public GameObject chargerPrefab;
-  public GameObject spitterPrefab;
-  public GameObject flyerPrefab;
+    public GameObject platformPrefab;
+    public GameObject playerPrefab;
+    public GameObject chargerPrefab;
+    public GameObject spitterPrefab;
+    public GameObject flyerPrefab;
 
-  public GameObject floor;
-  public GameObject leftWall;
-  public GameObject rightWall;
-  public GameObject ceiling;
+    public GameObject floor;
+    public GameObject leftWall;
+    public GameObject rightWall;
+    public GameObject ceiling;
 
-  // Note some may be culled.
-  public int estimatePlatformNumber;
-  public float maxPlatformHeight;
+    // Note some may be culled.
+    public int estimatePlatformNumber;
+    public float maxPlatformHeight;
 
-  public int enemyNumber;
+    public int enemyNumber;
 
-  private float gridHeight;
-  private float gridWidth;
+    private float gridHeight;
+    private float gridWidth;
 
-  private List<GameObject> platforms;
+    private List<GameObject> platforms;
 
-  private GameObject playerSpawnPlatform;
+    private GameObject playerSpawnPlatform;
 
-  void Start()
-  {
-    Camera camera = Camera.main;
-    float frustrumHeight = 2.0f * camera.orthographicSize;
-    float frustrumWidth = frustrumHeight * camera.aspect;
-    gridHeight = frustrumHeight;
-    gridWidth = frustrumWidth;
+    void Start()
+    {
+        Camera camera = Camera.main;
+        float frustrumHeight = 2.0f * camera.orthographicSize;
+        float frustrumWidth = frustrumHeight * camera.aspect;
+        gridHeight = frustrumHeight;
+        gridWidth = frustrumWidth;
 
-    platforms = new List<GameObject>();
+        platforms = new List<GameObject>();
 
-    GeneratePlatforms();
-    StartCoroutine(CheckObjectsAreStill());
+        GeneratePlatforms();
+        StartCoroutine(CheckObjectsAreStill());
         Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Enemies"), LayerMask.NameToLayer("Enemies"), true);
     }
 
-  private void GeneratePlatforms()
-  {
-    float width = gridWidth * 0.5f;
-    float height = gridHeight * 0.5f;
-    float platX = 0.0f;
-    float platY = 0.0f;
+    private void GeneratePlatforms()
+    {
+        float width = gridWidth * 0.5f;
+        float height = gridHeight * 0.5f;
+        float platX = 0.0f;
+        float platY = 0.0f;
 
-    float radius = platformPrefab.GetComponent<CircleCollider2D>().radius;
-    width -= radius;
-    height -= radius;
+        float radius = platformPrefab.GetComponent<CircleCollider2D>().radius;
+        width -= radius;
+        height -= radius;
 
-    // Instantiate platform colliders with random positions.
-    for (int i = 0; i < estimatePlatformNumber; i++) {
-      platX = Random.Range(-width, width);
-      platY = Random.Range(-height, height);
-      GameObject platformCollider = Instantiate(platformPrefab, new Vector3(platX, platY, 0), Quaternion.identity) as GameObject;
-      platforms.Add(platformCollider);
-    }
-  }
-
-  IEnumerator CheckObjectsAreStill()
-  {
-    Debug.Log("Checking");
-    bool allAsleep = false;
-    while (!allAsleep) {
-      allAsleep = true;
-      Debug.Log("NOT SLEEPING");
-      for (int i = 0; i < platforms.Count; i++) {
-        if (!platforms[i].GetComponent<Rigidbody2D>().IsSleeping()) {
-          allAsleep = false;
-          yield return null;
-          break;
+        // Instantiate platform colliders with random positions.
+        for (int i = 0; i < estimatePlatformNumber; i++) {
+            platX = Random.Range(-width, width);
+            platY = Random.Range(-height, height);
+            GameObject platformCollider = Instantiate(platformPrefab, new Vector3(platX, platY, 0), Quaternion.identity) as GameObject;
+            platforms.Add(platformCollider);
         }
-      }
     }
-    Debug.Log("Asleep");
 
-    CullPlatformElements();
-    enableBoundaries();
-    spawnPlayer();
+    IEnumerator CheckObjectsAreStill()
+    {
+        Debug.Log("Checking");
+        bool allAsleep = false;
+        while (!allAsleep) {
+            allAsleep = true;
+            Debug.Log("NOT SLEEPING");
+            for (int i = 0; i < platforms.Count; i++) {
+                if (!platforms[i].GetComponent<Rigidbody2D>().IsSleeping()) {
+                    allAsleep = false;
+                    yield return null;
+                    break;
+                }
+            }
+        }
+        Debug.Log("Asleep");
+
+        CullPlatformElements();
+        enableBoundaries();
+        spawnPlayer();
         spawnEnemies("Charger");//PlayerPrefs.GetString("Enemy"));
-  }
-
-  private void CullPlatformElements()
-  {
-    List<GameObject> plats = new List<GameObject>();
-    for (int i = 0; i < platforms.Count; i++) {
-      float platX = platforms[i].GetComponent<Transform>().transform.position.x;
-      float platY = platforms[i].GetComponent<Transform>().transform.position.y;
-      float width = gridWidth * 0.5f;
-      float height = gridHeight * 0.5f;
-
-      // Destroy platforms outside of the bounds.
-      if (platX >= width || platX <= -width || platY >= maxPlatformHeight || platY <= -height) {
-        Destroy(platforms[i]);
-      } else {
-        Destroy(platforms[i].GetComponent<Rigidbody2D>());
-        platforms[i].GetComponent<CircleCollider2D>().enabled = false;
-        platforms[i].GetComponentInChildren<SpriteRenderer>().enabled = true;
-        plats.Add(platforms[i]);
-      }
-    }
-    Debug.Log("Fixed");
-    platforms = plats;
-  }
-
-  //
-  private void enableBoundaries()
-  {
-    floor.SetActive(true);
-    leftWall.SetActive(true);
-    rightWall.SetActive(true);
-    ceiling.SetActive(true);
-  }
-
-  // Spawn the player on a platform.
-  private void spawnPlayer()
-  {
-    int index = 0;
-    //do {
-    index = Random.Range(0, platforms.Count);
-    float platX = platforms[index].transform.position.x;
-    float platY = platforms[index].transform.position.y;
-    //} while ();
-    playerSpawnPlatform = platforms[index];
-    Instantiate(playerPrefab, new Vector3(platX, platY, 0), Quaternion.identity);
-  }
-
-  //
-  private void spawnEnemies(string enemyType)
-  {
-    // Charger.
-    if (enemyType == "Charger") {
-      int index = 0;
-      for (int i = 0; i < enemyNumber; i++) {
-        if (index == platforms.Count) {
-          index = 0;
-        }
-
-        // Do not spawn enemies on the player's starting platform.
-        if (platforms[index] == playerSpawnPlatform) {
-          // Keep the index value for the enemy the same.
-          i--;
-          index++;
-          continue;
-        }
-
-        float x = platforms[index].transform.position.x;
-        float y = platforms[index].transform.position.y;
-        Instantiate(chargerPrefab, new Vector3(x, y + 0.5f, 0), Quaternion.identity);
-        index++;
-      }
     }
 
-    // Spitter.
-    if (enemyType == "Spitter") {
-      int index = 0;
-      for (int i = 0; i < enemyNumber; i++) {
-        if (index == platforms.Count) {
-          index = 0;
-        }
+    private void CullPlatformElements()
+    {
+        List<GameObject> plats = new List<GameObject>();
+        for (int i = 0; i < platforms.Count; i++) {
+            float platX = platforms[i].GetComponent<Transform>().transform.position.x;
+            float platY = platforms[i].GetComponent<Transform>().transform.position.y;
+            float width = gridWidth * 0.5f;
+            float height = gridHeight * 0.5f;
 
-        // Do not spawn enemies on the player's starting platform.
-        if (platforms[index] == playerSpawnPlatform) {
-          // Keep the index value for the enemy the same.
-          i--;
-          index++;
-          continue;
+            // Destroy platforms outside of the bounds.
+            if (platX >= width || platX <= -width || platY >= maxPlatformHeight || platY <= -height) {
+                Destroy(platforms[i]);
+            } else {
+                Destroy(platforms[i].GetComponent<Rigidbody2D>());
+                platforms[i].GetComponent<CircleCollider2D>().enabled = false;
+                platforms[i].GetComponentInChildren<SpriteRenderer>().enabled = true;
+                plats.Add(platforms[i]);
+            }
         }
-
-        float x = platforms[index].transform.position.x;
-        float y = platforms[index].transform.position.y;
-        Instantiate(spitterPrefab, new Vector3(x, y + 0.5f, 0), Quaternion.identity);
-        index++;
-      }
+        Debug.Log("Fixed");
+        platforms = plats;
     }
 
-    // Flyer.
-    if (enemyType == "Flyer") {
-      for (int i = 0; i < enemyNumber; i++) {
-        float x = Random.Range(-gridWidth * 0.5f, gridWidth * 0.5f);
-        float y = Random.Range(maxPlatformHeight, gridHeight * 0.5f);
-        Instantiate(flyerPrefab, new Vector3(x, y, 0), Quaternion.identity);
-      }
+    //
+    private void enableBoundaries()
+    {
+        floor.SetActive(true);
+        leftWall.SetActive(true);
+        rightWall.SetActive(true);
+        ceiling.SetActive(true);
     }
-  }
+
+    // Spawn the player on a platform.
+    private void spawnPlayer()
+    {
+        int index = 0;
+        //do {
+        index = Random.Range(0, platforms.Count);
+        float platX = platforms[index].transform.position.x;
+        float platY = platforms[index].transform.position.y;
+        //} while ();
+        playerSpawnPlatform = platforms[index];
+        Instantiate(playerPrefab, new Vector3(platX, platY, 0), Quaternion.identity);
+    }
+
+    public float spawnThreshold = 0.2f;
+    public float doubleSpawnThreshold = 0.8f;
+    //
+    private void spawnEnemies(string enemyType)
+    {
+        int enemies = 0;
+        // Charger.
+        if (enemyType == "Charger") {
+            for (int i = 0; i < platforms.Count; i++) {
+                // Skip the player platform.
+                if (platforms[i] == playerSpawnPlatform) {
+                    continue;
+                }
+
+                float rand = Random.value;
+                if (rand >= spawnThreshold || enemies == 0) {
+                    float x = platforms[i].transform.position.x;
+                    float y = platforms[i].transform.position.y;
+
+                    if (rand >= doubleSpawnThreshold) {
+                        Instantiate(chargerPrefab, new Vector3(x, y + 0.5f, 0), Quaternion.identity);
+                        Instantiate(chargerPrefab, new Vector3(x, y + 0.5f, 0), Quaternion.identity);
+                        enemies += 2;
+                    } else {
+                        Instantiate(chargerPrefab, new Vector3(x, y + 0.5f, 0), Quaternion.identity);
+                        enemies++;
+                    }
+                }
+            }
+        }
+
+        // Spitter.
+        if (enemyType == "Spitter") {
+            for (int i = 0; i < platforms.Count; i++) {
+                // Skip the player platform.
+                if (platforms[i] == playerSpawnPlatform) {
+                    continue;
+                }
+
+                float rand = Random.value;
+                if (rand >= spawnThreshold || enemies == 0) {
+                    float x = platforms[i].transform.position.x;
+                    float y = platforms[i].transform.position.y;
+
+                    if (rand >= doubleSpawnThreshold) {
+                        Instantiate(chargerPrefab, new Vector3(x, y + 0.5f, 0), Quaternion.identity);
+                        Instantiate(chargerPrefab, new Vector3(x, y + 0.5f, 0), Quaternion.identity);
+                        enemies += 2;
+                    } else {
+                        Instantiate(spitterPrefab, new Vector3(x, y + 0.5f, 0), Quaternion.identity);
+                        enemies++;
+                    }
+                }
+            }
+        }
+
+        // Flyer.
+        if (enemyType == "Flyer") {
+            for (int i = 0; i < enemyNumber; i++) {
+                float x = Random.Range(-gridWidth * 0.5f, gridWidth * 0.5f);
+                float y = Random.Range(maxPlatformHeight, gridHeight * 0.5f);
+                Instantiate(flyerPrefab, new Vector3(x, y, 0), Quaternion.identity);
+            }
+        }
+    }
 }
