149bdac3921bef9ccc15b2b17e8cd8326d6806f1
Adam Joyce <adamjoyce@outlook.com>
2016-03-13 17:19:57 +0000

    Tracking all the new files i failed to add

---
+++ Assets/Scripts/EarthLevelSpawns.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EarthLevelSpawns : MonoBehaviour {
+
+    public GameObject chargerBossPrefab;
+    public GameObject spitterBossPrefab;
+    public GameObject flyerBossPrefab;
+
+    // Use this for initialization
+    void Start() {
+        spawnBoss(PlayerPrefs.GetString("Enemy"));
+    }
+
+    //
+    private void spawnBoss(string enemyType)
+    {
+        if (enemyType == "Charger") {
+            Instantiate(chargerBossPrefab, new Vector3(2.0f, 0, 0), Quaternion.identity);
+        } else if (enemyType == "Spitter") {
+            Instantiate(spitterBossPrefab, new Vector3(2.0f, 0, 0), Quaternion.identity);
+        } else {
+            // Flyer.
+            Instantiate(flyerBossPrefab, new Vector3(2.0f, 2.0f, 0), Quaternion.identity);
+        }
+    }
+}
